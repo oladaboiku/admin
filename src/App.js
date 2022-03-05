9480a764273a84ec64946b732113781dbf1bfbbe
Donald O. Daboiku <oladaboiku@gmail.com>
2022-03-05 18:21:05 +0100

    topbar commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
     <div className="App">
-      
+     <Topbar/>
     </div>
   );
 }
---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./topbar.css";
+
+function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Flipkart</span>
+        </div>
+        <div className="topRight"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
