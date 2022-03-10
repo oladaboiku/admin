c87a1b8b399bb33c8e52244f3de1fe1bc7592640
Donald O. Daboiku <oladaboiku@gmail.com>
2022-03-10 11:48:07 +0100

    home page done

--- src/components/widgetLg/WidgetLg.js
+++ src/components/widgetLg/WidgetLg.js
@@ -11,13 +11,13 @@ function WidgetLg() {
         <h3 className="widgetLgTitle">Latest Transcations</h3>
         <table className="widgetLgTable">
           <tr className="widgetLgTr">
-            <th className="widget">Customer</th>
-            <th className="widget">Date</th>
-            <th className="widget">Amount</th>
-            <th className="widget">Status</th>
+            <th className="widgetTh">Customer</th>
+            <th className="widgetTh">Date</th>
+            <th className="widgetTh">Amount</th>
+            <th className="widgetTh">Status</th>
           </tr>
           <tr className="widgetLgTr">
-            <td className="widgetLgUser">
+            <td className="widgetUser">
               <img src="https://images.pexels.com/photos/10663495/pexels-photo-10663495.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" className="widgetLgImg" />
               <span className="widgetLgName">Susan Carol</span>
             </td>
@@ -28,34 +28,34 @@ function WidgetLg() {
             </td>
           </tr>
           <tr className="widgetLgTr">
-            <td className="widgetLgUser">
+            <td className="widgetUser">
               <img src="https://images.pexels.com/photos/10663495/pexels-photo-10663495.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" className="widgetLgImg" />
               <span className="widgetLgName">Susan Carol</span>
             </td>
             <td className="widgetLgDate">2 Mar 2022</td>
-            <td className="widgetLgAm">$122.00</td>
+            <td className="widgetLgAmount">$122.00</td>
             <td className="widgetLgStatus">
               <Button type="Declined"/>
             </td>
           </tr>
           <tr className="widgetLgTr">
-            <td className="widgetLgUser">
+            <td className="widgetUser">
               <img src="https://images.pexels.com/photos/10663495/pexels-photo-10663495.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" className="widgetLgImg" />
               <span className="widgetLgName">Susan Carol</span>
             </td>
             <td className="widgetLgDate">2 Mar 2022</td>
-            <td className="widgetLgAm">$122.00</td>
+            <td className="widgetLgAmount">$122.00</td>
             <td className="widgetLgStatus">
               <Button type="Pending"/>
             </td>
           </tr>
           <tr className="widgetLgTr">
-            <td className="widgetLgUser">
+            <td className="widgetUser">
               <img src="https://images.pexels.com/photos/10663495/pexels-photo-10663495.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" className="widgetLgImg" />
               <span className="widgetLgName">Susan Carol</span>
             </td>
             <td className="widgetLgDate">2 Mar 2022</td>
-            <td className="widgetLgAm">$122.00</td>
+            <td className="widgetLgAmount">$122.00</td>
             <td className="widgetLgStatus">
               <Button type="Approved"/>
             </td>
