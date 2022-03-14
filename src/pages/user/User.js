224e2e9735b9a8563c7c29d5f5d392cfedf85ab2
Donald O. Daboiku <oladaboiku@gmail.com>
2022-03-15 00:53:00 +0100

    user page

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./user.css";
-import {  PermIdentity, CalendarToday, PhoneAndroid, MailOutline, LocationSearching } from "@mui/icons-material";
+import {  PermIdentity, CalendarToday, PhoneAndroid, MailOutline, LocationSearching, Publish } from "@mui/icons-material";
 
 function User() {
   return (
@@ -30,24 +30,77 @@ function User() {
             </div>
             <div className="userShowInfo">
               <CalendarToday className="userShowIcon"/>
-            <span className="userShowInfoDate">1 25 1998</span>
+            <span className="userShowInfoTitle">1 25 1998</span>
             </div>
+            <span className="userShowTitle">Contact Details</span>
             <div className="userShowInfo">
               <PhoneAndroid className="userShowIcon"/>
-              <span className="userShowInfoPhone">+1 234 456 78</span>
+              <span className="userShowInfoTitle">+1 234 456 78</span>
             </div>
             <div className="userShowInfo">
               <MailOutline  className="userShowIcon"/>
-              <span className="userShowInfoMail">annabeck99@gmail.com</span>
+              <span className="userShowInfoTitle">annabeck99@gmail.com</span>
             </div>
             <div className="userShowInfo">
               <LocationSearching  className="userShowIcon"/>
-              <span className="userShowInfoLocation">New York | USA</span>
+              <span className="userShowInfoTitle">New York | USA</span>
             </div>
           </div>
         </div>
+      <div className="userUpdate">
+        <span className="userUpdateTitle">Edit</span>
+        <form  className="userUpdateForm">
+          <div className="userUpdateLeft">
+            <div className="userUpdateItem">
+              <label>Username</label>
+              <input type="text" 
+              placeholder="annabeck99" 
+              className="userUpdateInput" 
+              />
+            </div>
+            <div className="userUpdateItem">
+              <label>Full Name</label>
+              <input type="text" 
+              placeholder="Anna Becker" 
+              className="userUpdateInput" 
+              />
+            </div>
+            <div className="userUpdateItem">
+              <label>Email</label>
+              <input type="text" 
+              placeholder="annabeck99@gmail.com" 
+              className="userUpdateInput" 
+              />
+            </div>
+            <div className="userUpdateItem">
+              <label>Phone</label>
+              <input type="text" 
+              placeholder="+1 234 567 89" 
+              className="userUpdateInput" 
+              />
+            </div>
+            <div className="userUpdateItem">
+              <label>Address</label>
+              <input type="text" 
+              placeholder="New York | USA" 
+              className="userUpdateInput" 
+              />
+            </div>
+          </div>
+          <div className="userUpdateRight">
+            <div className="userUpdateUpload">
+              <img src="https://images.pexels.com/photos/10832112/pexels-photo-10832112.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+               alt=""
+               className="userUpdateImg" 
+               />
+              <label htmlFor="file"><Publish className="userUpdateIcon"/></label>
+              <input type="file"  id="file" style={{display:"none"}}/>
+            </div>
+            <button className="userUpdateButton">Update</button>
+          </div>
+        </form>
       </div>
-      <div className="userUpdate"></div>
+      </div>
     </div>
   );
 }
